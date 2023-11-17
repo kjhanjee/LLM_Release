Fixed issues with attention masking -- replace triu with tril

model/LLMLayer.py
@@ -50,7 +50,7 @@ class LLMLayer(torch.nn.Module):
         self.token_embedding = EmbeddingLayer(embedding_dimension, number_of_tokens)
         self.positional_encoding = PositionalEncodingLayer(embedding_dimension)
         self.layer_normalization = torch.nn.LayerNorm(embedding_dimension)
-        self.decoder = torch.nn.ModuleList([DecoderStackLayer(
+        self.decoder_stacks = torch.nn.ModuleList([DecoderStackLayer(
             embedding_dimension=embedding_dimension,
             number_of_layers=number_of_layers,
             number_of_heads=number_of_heads,
@@ -60,7 +60,7 @@ class LLMLayer(torch.nn.Module):
             max_stack = decoder_stacks - 1,
             training=training
         ).to('cuda',non_blocking=True) for i in range(decoder_stacks)])
-        self.lm_head = LMHeadLayer(4*embedding_dimension, number_of_tokens).to('cuda',non_blocking=True)
+        self.lm_head = LMHeadLayer(4*embedding_dimension, number_of_tokens)
     
     @custom_fwd(cast_inputs=torch.float16)
     def forward(self, x:torch.Tensor, mask:torch.Tensor):
@@ -77,7 +77,8 @@ class LLMLayer(torch.nn.Module):
         positional_encoding = self.positional_encoding(token_embeddings)
         positional_encoding_normalized = self.layer_normalization(positional_encoding)
         decoder_outputs = positional_encoding_normalized
-        for decoder in self.decoder:
-            decoder_outputs = decoder(decoder_outputs.to('cuda',non_blocking=True), mask.to('cuda',non_blocking=True))
-        lm_head_outputs = self.lm_head(decoder_outputs)
-        return lm_head_outputs.to('cpu')
+        for decoder_stack in self.decoder_stacks:
+            decoder_outputs = decoder_stack(decoder_outputs.to('cuda',non_blocking=True), mask.to('cuda',non_blocking=True))
+            mask = torch.ones(mask.size(), device = 'cuda')
+        lm_head_outputs = self.lm_head(decoder_outputs.to('cpu'))
+        return lm_head_outputs

model/SelfAttention.py
@@ -44,11 +44,11 @@ class SelfAttention(torch.nn.Module):
         value = self.v_layer(x)
         attention_weights = matmul(query, key.transpose(-2, -1)) # Creating Key . Query
         attention_weights = attention_weights / np.sqrt(self.head_dimension) # Weight Scaling
-        temp_mask = ones((attention_weights.shape[0],attention_weights.shape[1],attention_weights.shape[2]), dtype = torch.float16, device='cuda') # Temporary Mask object with all 1s
+        temp_mask = ones((attention_weights.shape[0],attention_weights.shape[1],attention_weights.shape[2]), dtype = torch.float32, device='cuda') # Temporary Mask object with all 1s
         for index, item in enumerate(temp_mask):
             for index2 in range(len(item)):
                 temp_mask[index][index2] = mask[index]
-            temp_mask[index] = temp_mask[index].triu(0) # Sequence X Sequence Matrix with Mask values as 0 for upper diagonal
+            temp_mask[index] = temp_mask[index].tril(0) # Sequence X Sequence Matrix with Mask values as 0 for upper diagonal
         attention_weights = attention_weights.masked_fill(temp_mask == 0, -1e22) # Very Small number assignment so that further processing is not done on the Attention Weight
         attention_scores = self.softmax(attention_weights) # Weight to scores
         out = bmm(attention_scores, value) # Batch matric product of Attention scores and the input

utils/trainer.py
@@ -169,16 +169,17 @@ class Trainer:
                             for item in tensor:
                                 if not int(np.argmax(item.detach().cpu().numpy()))==24:
                                     outputs[index].append(int(np.argmax(item.detach().cpu().numpy())))
-
                         
-                        
-                        print(f'\n\nStep {step} Loss: ', loss.item())
                         print(f"\n\nStep {step} Output: "+str(self.tokenizer.decode_batch(outputs,skip_special_tokens=False)))
+                        print(f"\n\n Output Dtype: {out.dtype}")
                         
                         loss.backward()                     
                         torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
                         self.optimizer.step()
-                        self.optimizer.zero_grad(set_to_none=True)  
+                        self.optimizer.zero_grad(set_to_none=True)
+                        
+                        print(f'\n\nStep {step} Loss: ', loss.item())
+                        
                         
                         if step % accum_iter == 0:
                             PATH = config['checkpoint_path']+"model"+str(step)+".pt"